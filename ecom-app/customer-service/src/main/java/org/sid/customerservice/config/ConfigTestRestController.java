69a057ff58de4ac8320c9eeb4e917df163a94aac
hassan aitdadali <hassanaitdadali@gmail.com>
2025-01-06 12:45:39 +0100

    update

--- ecom-app/customer-service/src/main/java/org/sid/customerservice/config/ConfigTestRestController.java
+++ ecom-app/customer-service/src/main/java/org/sid/customerservice/config/ConfigTestRestController.java
@@ -9,16 +9,16 @@ import java.util.Map;
 
 @RestController
 public class ConfigTestRestController {
-    @Value("${global.params.p1:defaultP1}")
+    @Value("${global.params.p1}")
     private String p1;
-    @Value("${global.params.p2:defaultP2}")
+    @Value("${global.params.p2}")
     private String p2;
 
     @GetMapping("/testConfig1")
     public Map<String,String> configTest(){
+       return Map.of("p1", p1, "p2", p2);
+//        System.out.println("p1: " + p1); // Print the value of p1
+//        System.out.println("p2: " + p2); // Print the value of p2
 //        return Map.of("p1", p1, "p2", p2);
-        System.out.println("p1: " + p1); // Print the value of p1
-        System.out.println("p2: " + p2); // Print the value of p2
-        return Map.of("p1", p1, "p2", p2);
     }
 }
